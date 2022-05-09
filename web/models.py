66d0fe08bc44af1294e8769696f3a06f41366f6c
David Lesman <david.lesman@icloud.com>
2022-05-09 23:04:33 +0200

    Created password reset system using smtp and django builtins.

--- web/models.py
+++ web/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    location = models.CharField(max_length=64, default="Antwerp")
 
 
 class Category(models.Model):
