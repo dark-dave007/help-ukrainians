6bd006419a38c50bfc3b4e842498d87674c95ea4
David Lesman <david.lesman@icloud.com>
2022-05-16 14:05:56 +0200

    Added meta tags

--- ukrainehelp/settings.py
+++ ukrainehelp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.environ["SECRET_KEY"])
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -120,10 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 
-STATIC = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
@@ -146,3 +146,4 @@ handler500 = "web.views.server_error_view"
 # HTTPS
 # CSRF_COOKIE_SECURE = True
 # SESSION_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
